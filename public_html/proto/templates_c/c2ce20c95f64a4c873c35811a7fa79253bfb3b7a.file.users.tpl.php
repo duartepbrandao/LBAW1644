88c237508d88af62f26acffb3c47ade87c8cf5d3
João Fernandes <ojoaofernandes@gmail.com>
2017-04-19 18:30:10 +0100

    Fix admin#users endpoint.

---
+++ public_html/proto/templates_c/c2ce20c95f64a4c873c35811a7fa79253bfb3b7a.file.users.tpl.php
@@ -0,0 +1,218 @@
+<?php /* Smarty version Smarty-3.1.15, created on 2017-04-19 19:29:50
+         compiled from "/var/www/LBAW1644/public_html/proto/templates/admin/users.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:110484501858f79e8e484e72-05096174%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'c2ce20c95f64a4c873c35811a7fa79253bfb3b7a' => 
+    array (
+      0 => '/var/www/LBAW1644/public_html/proto/templates/admin/users.tpl',
+      1 => 1492622976,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '110484501858f79e8e484e72-05096174',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.15',
+  'unifunc' => 'content_58f79e8e496ee4_42421392',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_58f79e8e496ee4_42421392')) {function content_58f79e8e496ee4_42421392($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ('common/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
+
+        <!-- Body Content -->
+        <div id="wrapper">
+
+            <?php echo $_smarty_tpl->getSubTemplate ('common/navigation.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
+
+            <div id="page-wrapper">
+                <div class="container-fluid">
+                    <!-- Page Heading -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">
+                                Área de Administração
+                            </h1>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+
+                    <div class="col-lg-12">
+                        <h3>Utilizadores</h3>
+                        <hr class="divider">
+
+                        <div class="table-responsive">
+                            <table id="newUsers" class="table table-hover table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Email</th>
+                                        <th>Permissões</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Gestor</td>
+                                        <td>
+                                        <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Operador</td>
+                                        <td>
+                                        <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                        <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Operador</td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Gestor</td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                        <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                    <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                        <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Operador</td>
+                                        <td>
+                                        <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                        <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Operador</td>
+                                        <td>
+                                        <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lorem ipsum dolor</td>
+                                        <td>lorempsum@mail.com</td>
+                                        <td>Utilizador</td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger">Banir</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+
+        </div>
+        <!-- /#wrapper -->
+
+        <?php echo $_smarty_tpl->getSubTemplate ('common/scripts.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
+
+        <!-- Custom Scripts -->
+        <script>
+            $(document).ready(function () {
+                $('#newUsers').dataTable();
+            });
+        </script>
+
+<?php echo $_smarty_tpl->getSubTemplate ('common/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
+<?php }} ?>
