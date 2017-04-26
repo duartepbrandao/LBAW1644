c72500b0a21b3dd6be732ea31b6838af83a38fe1
Duarte Brandão <duartepbrandao@hotmail.com>
2017-04-26 18:42:55 +0100

    duplicate email validation;passwords match validation

--- public_html/proto/actions/users/register.php
+++ public_html/proto/actions/users/register.php
@@ -5,7 +5,15 @@
   if (!$_POST['real_name'] || !$_POST['email']|| !$_POST['password']|| !$_POST['confirm_password']) {
     $_SESSION['error_messages'][] = 'All fields are mandatory';
     $_SESSION['form_values'] = $_POST;
-    header("Location: $BASE_URL" . 'pages/users/register.php');
+    header("Location: $BASE_URL" . 'pages/user/register.php');
+    exit;
+  }
+  
+  if($_POST['password'] != $_POST['confirm_password'])
+  {
+	$_SESSION['error_messages'][] = 'Passwords não coincidem';
+    $_SESSION['form_values'] = $_POST;
+    header("Location: $BASE_URL" . 'pages/user/register.php');
     exit;
   }
   
@@ -18,14 +26,14 @@
    
   } catch (PDOException $e) {
   
-    if (strpos($e->getMessage(), 'users_pkey') !== false) {
-      $_SESSION['error_messages'][] = 'Duplicate username';
-      $_SESSION['field_errors']['username'] = 'Username already exists';
+    if (strpos($e->getMessage(), 'utilizador_email_key') !== false) {
+      $_SESSION['error_messages'][] = 'Duplicate email';
+      $_SESSION['field_errors']['email'] = 'Email já se encontra registado';
     }
     else $_SESSION['error_messages'][] = 'Error creating user';
 
     $_SESSION['form_values'] = $_POST;
-    header("Location: $BASE_URL" . 'pages/users/register.php');
+    header("Location: $BASE_URL" . 'pages/user/register.php');
     exit;
   }
   $_SESSION['success_messages'][] = 'User registered successfully';  
--- public_html/proto/templates/common/header.tpl
+++ public_html/proto/templates/common/header.tpl
@@ -28,3 +28,14 @@
         <![endif]-->
     </head>
     <body>
+
+    <div id="error_messages">
+    {foreach $ERROR_MESSAGES as $error}
+      <div class="error">{$error}<a class="close" href="#">X</a></div>
+    {/foreach}
+    </div>
+    <div id="success_messages">
+    {foreach $SUCCESS_MESSAGES as $success}
+      <div class="success">{$success}<a class="close" href="#">X</a></div>
+    {/foreach}
+    </div>
